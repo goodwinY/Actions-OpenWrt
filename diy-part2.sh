13cc208c382347f962d33678d6e4c6b2e41ab2de
goodwinY <39821186+goodwinY@users.noreply.github.com>
2021-07-16 17:10:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,16 +18,16 @@
 #sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 #2．修改主机名 hostname 'soft_Routes''->hostname 'goodwin''
-#sed -i '/uci comit system/iuci set system.isystem[ ].hostname 'soft_Routes'' package/lean/default-settings/files/zzz-default-settings
+sed -i '/uci comit system/iuci set system.isystem[ ].hostname 'openwrt_goodwin'' package/lean/default-settings/files/zzz-default-settings
 
 #3．设置密码为空
-#sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 #4．版本号里显示一个自己的名字 /Lunseil build $(TZ=UTC-8 date "+%Y.%m.%d") @ Openwrt->Lunseil 2021 10 1 @Openwrt 
-#sed -i "s/Openwrt /Lunseil build $(TZ=UTC-8 date "+%Y.%m.%d") @ Openwrt /g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/Openwrt /Openwrt build $(TZ=UTC-8 date "+%Y.%m.%d") @ Openwrt /g" package/lean/default-settings/files/zzz-default-settings
 
 #5．修改插件名字
-#sed -i 's /"Turbo ACC 网络加速"/"网络加速"/g' package/lean/luci-app-sfe/po/zh-cn/sfe.po
+sed -i 's /"高级重启"/"关机"/g' package/lean/luci-app-sfe/po/zh-cn/sfe.po
 
 #6．修改上游DNS
 #sed -i "2i # network config" package/lean/default-settings/files/zzz-default-settings
