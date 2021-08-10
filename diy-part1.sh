346507f4b2c51ca182dcc50cf2dbf0f3bdacfdc1
goodwinY <39821186+goodwinY@users.noreply.github.com>
2021-08-10 19:58:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,30 +15,28 @@
 
 # netkeeper 创翼
 # sed -i '$a src-git diy https://github.com/CCnut/feed-netkeeper.git;LUCI-LUA-UCITRACK' feeds.conf.default
-#theme-argon 主题
-#cd package/lean  
-#rm -rf luci-theme-argon 
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
-#cd ../..
+#theme-argon 主题  
+#rm -rf package/lean/luci-theme-argon 
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/goodwin/luci-theme-argon
 
-#rm -rf package/luci-app-dockerman
+#rm -rf package/goodwin/luci-app-dockerman
 #rm -rf package/lean/luci-lib-docker
-#rm -rf package/luci-app-tcpdump
-#rm -rf package/luci-app-pushbot
-#rm -rf package/luci-app-poweroff
-#rm -rf package/luci-app-feed-netkeeper
+#rm -rf package/goodwin/luci-app-tcpdump
+#rm -rf package/goodwin/luci-app-pushbot
+#rm -rf package/goodwin/luci-app-poweroff
+#rm -rf package/goodwin/luci-app-feed-netkeeper
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package.git;main' feeds.conf.default
 #sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-#git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
-#git clone https://github.com/lisaac/luci-lib-docker package/lean/luci-lib-docker
-#git clone https://github.com/KFERMercer/luci-app-tcpdump.git package/luci-app-tcpdump
-#git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
-#git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
-#git clone -b LUCI-LUA-UCITRACK https://github.com/CCnut/feed-netkeeper package/luci-app-feed-netkeeper
+#git clone https://github.com/lisaac/luci-app-dockerman package/goodwin/luci-app-dockerman
+#git clone https://github.com/lisaac/luci-lib-docker package/goodwin/luci-lib-docker
+#git clone https://github.com/KFERMercer/luci-app-tcpdump.git package/goodwin/luci-app-tcpdump
+#git clone https://github.com/zzsj0928/luci-app-pushbot.git package/goodwin/luci-app-pushbot
+#git clone https://github.com/esirplayground/luci-app-poweroff package/goodwin/luci-app-poweroff
+#git clone -b LUCI-LUA-UCITRACK https://github.com/CCnut/feed-netkeeper package/goodwin/luci-app-feed-netkeeper
 
 #test
-sed -i '$a src-git goodwin https://github.com/281677160/openwrt-package' feeds.conf.default
+#sed -i '$a src-git goodwin https://github.com/281677160/openwrt-package' feeds.conf.default
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
