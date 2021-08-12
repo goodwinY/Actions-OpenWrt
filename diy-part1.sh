6576f04d8a400486cff2c1ff5b593ef419c45a3a
goodwinY <39821186+goodwinY@users.noreply.github.com>
2021-08-13 07:21:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 sed -i '$a src-git netkeeper https://github.com/CCnut/feed-netkeeper.git;LUCI-LUA-UCITRACK' feeds.conf.default
 
 #rm -rf package/goodwin/luci-app-tcpdump
-rm -rf package/goodwin/luci-app-pushbot
+#rm -rf package/goodwin/luci-app-pushbot
 rm -rf package/goodwin/luci-app-poweroff
 #rm -rf package/goodwin/luci-app-feed-netkeeper
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
@@ -31,6 +31,6 @@ sed -i '$a src-git lisaacdockerlib https://github.com/lisaac/luci-lib-docker' fe
 #git clone https://github.com/lisaac/luci-app-dockerman package/goodwin/luci-app-dockerman
 #git clone https://github.com/lisaac/luci-lib-docker package/goodwin/luci-lib-docker
 #git clone https://github.com/KFERMercer/luci-app-tcpdump.git package/goodwin/luci-app-tcpdump
-git clone https://github.com/zzsj0928/luci-app-pushbot.git package/goodwin/luci-app-pushbot
+#git clone https://github.com/zzsj0928/luci-app-pushbot.git package/goodwin/luci-app-pushbot
 git clone https://github.com/esirplayground/luci-app-poweroff package/goodwin/luci-app-poweroff
 #git clone -b LUCI-LUA-UCITRACK https://github.com/CCnut/feed-netkeeper package/goodwin/luci-app-feed-netkeeper
