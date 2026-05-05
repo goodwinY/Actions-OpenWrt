946e3efed4083a7a509ca3b9bf833d695a327165
goodwinY <39821186+goodwinY@users.noreply.github.com>
2026-05-05 23:31:22 +0800

    Update diy-part1-jiayong.sh

--- diy_script/diy-part1-jiayong.sh
+++ diy_script/diy-part1-jiayong.sh
@@ -12,7 +12,7 @@
 
 # Uncomment a feed source
 sed -i "/helloworld/d" "feeds.conf.default"
-#echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 
 # Add a feed source
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
