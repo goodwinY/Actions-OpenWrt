05abed998135d77ee67e21b9f016ecf75bf62a4b
goodwinY <39821186+goodwinY@users.noreply.github.com>
2026-09-24 23:41:05 +0800

    Update diy-part1-jiayong.sh

--- diy_script/diy-part1-jiayong.sh
+++ diy_script/diy-part1-jiayong.sh
@@ -17,6 +17,6 @@ echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf
 # Add a feed source
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git istore https://github.com/linkease/istore;main' feeds.conf.default
+#sed -i '$a src-git istore https://github.com/linkease/istore;main' feeds.conf.default
 
 
